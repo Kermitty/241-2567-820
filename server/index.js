c31d9897c4d59681b913b4326345d3573cd0cf61
Kermitty <nithit.sa@ku.th>
2025-02-11 14:33:05 +0700

    edit git

---
+++ server/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+const port = 8000;
+
+// เก็บ user
+let users = []
+let counter = 1
+
+/* GET /users สำหรับ get users ทั้งหมด
+POST /user สำหรับสร้าง user ใหม่บันทึกเข้าไป
+PUT /user/:id สำหรับแก้ไข user รายคนที่ต้องการบันทึก
+GET /user/:id สำหรับดึงข้อมูล user รายคน
+PUT /user/:id สำหรับแก้ไข user รายคนที่ต้องการบันทึก
+DELETE /user/:id สำหรับลบ user รายคนที่ต้องการลบ
+*/
+// path = GET /users5
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+  
+
+// path = POST /user
+app.post('/user', (req, res) => {
+  let user = req.body;
+  user.id = counter
+  counter++;
+  users.push(user);
+  res.json({
+    message : "User created",
+    user : user
+  });
+});
+
+//path = PUT /user/:id
+app.put('/user/:id', (req, res) => {
+  let id = req.params.id;
+  let updateUser = req.body
+  //หา user จาก id ที่ส่งมา
+  let selectedIndex = users.findIndex(user => user.id == id);
+  //update user นั้น
+  if (updateUser.firstname) {
+    users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
+  }
+  if (updateUser.lastname) {
+  users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
+  }
+  res.json({
+    message : "User updated",
+    data: {
+      user: updateUser,
+      indexUpdate: selectedIndex
+    }
+  });
+}
+);
+//Path = DELETE /user/:id
+app.delete('/user/:id', (req, res) => {
+  let id = req.params.id
+  //หา index ของ user ที่ต้องการลบ
+  let selectedIndex = users.findIndex(user => user.id == id);
+  
+  users.splice(selectedIndex, 1);
+  res.json({
+    message : "Delete Completed",
+    indexDelete : selectedIndex
+  });
+});
+
+app.listen(port, (req, res) => {
+    console.log('Server is running on port' + port);
+});
+
