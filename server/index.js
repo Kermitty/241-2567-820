4cc1ac81a2ad25cd084dfcb0e678ba716c2620b0
Kermitty <nithit.sa@ku.th>
2025-03-09 15:16:38 +0700

    edit git

--- server/index.js
+++ server/index.js
@@ -42,6 +42,28 @@ const initmysql = async () => {
     }
     
 })*/
+const validateData = (userData) => {
+    let errors = []
+    if(!userData.firstName) {
+        errors.push('กรุณากรอกชื่อ')
+    }
+    if(!userData.lastName) {
+        errors.push('กรุณากรอกนามสกุล')
+    }
+    if(!userData.age) {
+        errors.push('กรุณากรอกอายุ')
+    }
+    if(!userData.gender) {
+        errors.push('กรุณาเลือกเพศ')
+    }
+    if(!userData.interests) {
+        errors.push('กรุณาเลือกความสนใจ')
+    }
+    if(!userData.description) {
+        errors.push('กรุณากรอกข้อมูลตัวเอง')
+    }
+    return errors
+}
 
 // path = GET /users
 app.get('/users', async (req, res) => {
@@ -53,16 +75,26 @@ app.get('/users', async (req, res) => {
 app.post('/users', async (req, res) => {
     try{
         let user = req.body;
+        const errors = validateData(user)
+        if(errors.length > 0) {
+            throw {
+                message : 'กรุญากรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            }
+        }
+
         const results = await conn.query('INSERT INTO users SET ?', user)
         res.json({
             message: 'User created',
             data: results[0]
         })
     }catch(error){ // ถ้าเกิด error ให้ส่งข้อมูลกลับไปที่ client
+        const errorMessage = error.message || 'something went wrong'
+        const errors = error.errors || []
         console.error('errorMessge',error.message)
         res.status(500).json({
-            message: 'something went wrong',
-            errorMessge: error.message
+            message: errorMessage,
+            errors : errors
         }) // ส่งข้อมูลกลับไปที่ results ในรูปแบบ json
     }
 
